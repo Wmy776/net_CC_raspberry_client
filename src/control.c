a14a8fc1ba36e9b4673474823f04dfcedd189078
Wmy776 <784485305@qq.com>
2017-05-08 17:32:39 +0800

    change the file structure and edit the Makefile

---
+++ src/control.c
@@ -0,0 +1,24 @@
+#include "../include/includes.h"
+
+void send2control(int Device)
+{
+    switch(buffer[0])
+    {
+        case 0x01:
+            send_dirction(Device,buffer[1],buffer[2]);
+            break;
+        case 0x02:
+            send_engine(Device,buffer[1],buffer[2]);
+            break;
+        case 0x03:
+            send_backEngine(Device,buffer[1],buffer[2]);
+            break;
+        case 0x04:
+            if(buffer[1] == 0xCD)
+                send_ramX(Device,buffer[2]);
+            break;
+        case 0x05:
+            if(buffer[1] == 0xEF)
+                send_ramY(Device,buffer[2]);
+    }
+}
